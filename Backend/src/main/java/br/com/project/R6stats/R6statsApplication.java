93705be3679ede9949d6576a4ceaff4657484e27
vJhonatan <jhonatan99653@gmail.com>
2025-02-15 15:24:36 -0300

    initial commit

---
+++ Backend/src/main/java/br/com/project/R6stats/R6statsApplication.java
@@ -0,0 +1,13 @@
+package br.com.project.R6stats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class R6statsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(R6statsApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/br/com/project/R6stats/R6statsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.project.R6stats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class R6statsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
